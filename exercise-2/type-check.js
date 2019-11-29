ea4fcccad811cea8ec19403798bfc6c2565776c2
sami96 <sami@edzo.ai>
2019-11-29 16:18:56 +0100

    Exercise 2 type check v2

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,12 +1,49 @@
 function type_check_v1(arg1, arg2) {
-  let arg2LC = arg2.toLowerCase();
-  return typeof arg1 === arg2LC
-    ? true
-    : arg2LC === "array" && arg1.length > 0
-    ? true
-    : arg2LC === "null" && arg1 === null
-    ? true
-    : arg2LC;
+  // if (typeof arg1 === arg2.toLowerCase()) {
+  //   if (arg2.toLowerCase() === "object" && Array.isArray(arg1)) return false;
+  //   if (arg2.toLowerCase() === "object" && arg1 === null) return false;
+  //   else return true;
+  // } else {
+  //   if (arg2.toLowerCase() === "array" && Array.isArray(arg1)) return true;
+  //   if (arg2.toLowerCase() === "null" && arg1 === null) return true;
+  //   else return false;
+  // }
+
+  switch (typeof arg1) {
+    case "object":
+      if (Array.isArray(arg1)) return type === "array";
+      if (arg1 === null) return type === "null";
+    default:
+      return typeof arg1 === arg2.toLowerCase();
+  }
 }
 
-console.log(type_check_v1(1, "Number"));
+function type_check_v2(arg1, object) {
+  for (key in object) {
+    switch (key) {
+      case "type":
+        if (!type_check_v1(arg1, object[key])) return false;
+        break;
+      case "value":
+        if (JSON.stringify(arg1) !== JSON.stringify(object[key])) return false;
+        break;
+      case "enum":
+        if (
+          object[key].filter(obj => {
+            JSON.stringify(arg1) !== JSON.stringify(obj);
+          })
+        )
+          return false;
+        break;
+    }
+  }
+  return true;
+}
+
+console.log(type_check_v1("aa", "string"));
+
+console.log(
+  type_check_v2({ prop1: 2 }, { type: "object", value: { prop1: 2 } })
+);
+
+// todo type_check_v3
